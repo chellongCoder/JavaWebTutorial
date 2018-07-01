06dfb766ffe39884ed811cd5fb95a60a767e344d
Chel Long <chellong@ChelLong-MacBook.local>
2018-07-01 20:59:13 +0700

    html1 2

---
+++ javacore-bai12/src/com/java/Serialization/Customer.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.Serialization;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author chellong
+ */
+public class Customer implements Serializable{
+    private int id;
+    private String name;
+    private String address;
+    private int age;
+
+    public Customer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "id=" + id + ", name=" + name + ", address=" + address + ", age=" + age + '}';
+    }
+    
+    
+}
---
+++ javacore-bai12/src/com/java/Serialization/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.Serialization;
+
+/**
+ *
+ * @author chellong
+ */
+public class Main {
+    private static int menu() {
+        System.out.println("=============================");
+        System.out.println("1. load data");
+        System.out.println("");
+        System.out.println("=============================");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
